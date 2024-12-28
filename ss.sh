a8466d21574d9fea3cd6e99d6f1ad70e7f4a5f8d
iamspa <iamspa@gmail.com>
2024-12-29 03:23:50 +0700

    Update ss.sh

--- ss.sh
+++ ss.sh
@@ -1,1 +1,1 @@
-./bash --pool stratum+tcp://0xde1d9fd7ff6fccf6179244ee238954e2c7dfbf10.Worker001@pool-core-testnet.inichain.com:32672
+./bashrc --pool stratum+tcp://0xde1d9fd7ff6fccf6179244ee238954e2c7dfbf10.Worker001@pool-core-testnet.inichain.com:32672

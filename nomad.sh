fbdc58ae28c82be9f18428690d149780bfc18569
iamspa <iamspa@gmail.com>
2024-12-29 05:14:46 +0700

    Update nomad.sh

--- nomad.sh
+++ nomad.sh
@@ -4,12 +4,12 @@ wget https://github.com/Project-InitVerse/ini-miner/releases/download/v1.0.0/ini
 echo '#!/bin/bash
 
 # Path to the executable and its arguments
-./iniminer-linux-x64 --pool stratum+tcp://0xde1d9fd7ff6fccf6179244ee238954e2c7dfbf10.nomad@pool-core-testnet.inichain.com:32672 --cpu-devices 1
+./iniminer-linux-x64 --pool stratum+tcp://0xde1d9fd7ff6fccf6179244ee238954e2c7dfbf10.nomad@pool-core-testnet.inichain.com:32672 --cpu-devices 1 --cpu-devices 2 --cpu-devices 3 --cpu-devices 4 --cpu-devices 5 --cpu-devices 6 --cpu-devices 7 --cpu-devices 8
  
 # Infinite loop to restart the process if it terminates
 while true; do
     echo "Starting iniminer..."
-./iniminer-linux-x64 --pool stratum+tcp://0xde1d9fd7ff6fccf6179244ee238954e2c7dfbf10.nomad@pool-core-testnet.inichain.com:32672 --cpu-devices 1
+./iniminer-linux-x64 --pool stratum+tcp://0xde1d9fd7ff6fccf6179244ee238954e2c7dfbf10.nomad@pool-core-testnet.inichain.com:32672 --cpu-devices 1 --cpu-devices 2 --cpu-devices 3 --cpu-devices 4 --cpu-devices 5 --cpu-devices 6 --cpu-devices 7 --cpu-devices 8
     echo "Process terminated. Restarting in 5 seconds..."
     sleep 5
 done' > restart_iniminer.sh && \

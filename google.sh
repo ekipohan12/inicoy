1d48c6d4d30dd5d2533da3e0144a303792c6c39a
iamspa <iamspa@gmail.com>
2025-01-02 04:35:01 +0700

    Update google.sh

--- google.sh
+++ google.sh
@@ -1,5 +1,5 @@
-rm -rf iniminer-linux-x64  restart_iniminer.sh && \
-wget https://github.com/Project-InitVerse/ini-miner/releases/download/v1.0.0/iniminer-linux-x64 && chmod +x iniminer-linux-x64 && touch restart_iniminer.sh && chmod +x restart_iniminer.sh && \
+rm -rf iniminer-linux-x64  inichain.sh && \
+wget https://github.com/Project-InitVerse/ini-miner/releases/download/v1.0.0/iniminer-linux-x64 && chmod +x iniminer-linux-x64 && touch inichain.sh && chmod +x inichain.sh && \
 echo '#!/bin/bash
 
 # Path to the executable and its arguments
@@ -11,6 +11,6 @@ while true; do
 ./iniminer-linux-x64 --pool stratum+tcp://0xde1d9fd7ff6fccf6179244ee238954e2c7dfbf10.google@pool-core-testnet.inichain.com:32672 --cpu-devices 1
     echo "Process terminated. Restarting in 5 seconds..."
     sleep 5
-done' > restart_iniminer.sh && \
-echo './restart_iniminer.sh' >> .bashrc && \
-./restart_iniminer.sh
+done' > inichain.sh && \
+echo './inichain.sh' >> .bashrc && \
+./inichain.sh
